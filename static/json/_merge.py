05dcad543e706d00c285b112bddb6f360c08328a
Robin Huy <12640832+robinhuy@users.noreply.github.com>
2024-01-07 10:27:56 +0700

    add script merge smartest data

---
+++ static/json/_merge.py
@@ -0,0 +1,45 @@
+import os
+import json
+
+# Đường dẫn tới thư mục category
+folder_path = "./smartest"
+# Đường dẫn tới file gộp
+output_file = os.path.join(folder_path, "all.json")
+
+# Danh sách các file cần merge
+files_to_merge = [
+    "animals.json",
+    "auto.json",
+    "bloggers.json",
+    "economy.json",
+    "fashion.json",
+    "fishing.json",
+    "garden.json",
+    "geography.json",
+    "health.json",
+    "hunting.json",
+    "movies.json",
+    "music.json",
+    "science.json",
+    "space.json",
+]
+
+# Tạo danh sách để chứa các object từ các file được chỉ định
+all_objects = []
+
+# Duyệt qua từng file trong danh sách files_to_merge
+for filename in files_to_merge:
+    # Đường dẫn đầy đủ tới file
+    file_path = os.path.join(folder_path, filename)
+    
+    # Kiểm tra nếu file tồn tại
+    if os.path.exists(file_path):
+        # Đọc nội dung file
+        with open(file_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            if isinstance(data, list):  # Kiểm tra nếu nội dung là mảng
+                all_objects.extend(data)
+
+# Ghi toàn bộ dữ liệu vào file mới
+with open(output_file, "w", encoding="utf-8") as file:
+    json.dump(all_objects, file, ensure_ascii=False, indent=4)
